fe7dddcd9f8e4a194a9de87f45e73b7e9101bcce
m.sjoquist <cocochino@gmail.com>
2019-09-18 11:19:51 -0400

    Cleanup of simple examples.

--- simple_01/simple2.js
+++ simple_01/simple2.js
@@ -17,29 +17,27 @@ const ejs = require('ejs')
 
 app.set('ejs', ejs.renderFile)
 
-console.log("Phrito")
-
 app.use(bodyParser.urlencoded({ extended: false }))
  
-//Send current directory's simple2.ejs file as a response to browser
-//View files must be in views folder
+//Send view directory's simple2.ejs file as a response to browser
+//With default value of TBD
 app.get("/", (req, res) => {
     res.render("./simple2.ejs",
         {
-            name: 'TBD',
-            message: 'TBD'
+            item: 'TBD',
+            price: 'TBD'
         })
 });
  
  
 app.post("/", (req, res) => {
     res.render('simple2.ejs', {
-        name: req.body.name,
-        message: req.body.message
+        item: req.body.item,
+        price: req.body.price
     })
 });
  
 //Start application and listen to port 2000
  app.listen(2000, () => {
-    console.log('start')
+    console.log('simple2.js has been started')
 })
